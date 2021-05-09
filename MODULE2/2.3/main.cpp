203e2d8a557d79af2ababf73072be960a03495b8
Mr.Void <72743789+kirtanlab@users.noreply.github.com>
2021-05-09 09:45:16 +0530

    main

--- MODULE2/2.3/main.cpp
+++ MODULE2/2.3/main.cpp
@@ -7,7 +7,7 @@ private:
 	char* name;
 public:
 	//Using Constructor
-	tolowr(const char* s = nullptr)
+	tolowr(const char* s)
 		:name{ nullptr } {
 		if (s == nullptr) {
 			name = new char[1];
@@ -18,35 +18,38 @@ public:
 			strcpy(name, s);
 		}
 	}
+	 //Destructor
+	~tolowr(){
+	    std::cout << "Destructor has been called" << std::endl;
+        delete [] name;
+    }
 
 
-	//making  string lower
+    //making  string lower
 	const char* tolower_fun() {
 		char* buff = new char[strlen(name) + 1];
 		strcpy(buff, name);
 		for (size_t i = 0; i < strlen(buff); ++i) {
 			name[i] = tolower(buff[i]);
 		}
+
 		return name;
 	}
+    void display();
 
-	//display method
-	void display()
+};
+
+ //display method
+	void tolowr::display()
 	{
 	    std::cout << "Given String: " << name << std::endl;
 	    std::cout << "Lowered string: " << tolower_fun() << std::endl;
+	    this->tolowr::~tolowr();
 	}
 
-	//Destructor
-	~tolowr(){
-	    std::cout << "Destructor has been called" << std::endl;
-        delete [] name;
-    }
-};
-
-
 int main() {
-    tolowr str{"KIRTAN"};
-    str.display();
+    tolowr* str = new tolowr{};
+    str->display();//display Method Includes destructor calling since we have declared our object using "new" keyword! so we have to remove that allocated storage!
+	str->display();//It will provide Garbage Data since we have deleted allocated Address!
 	return 0;
 }
